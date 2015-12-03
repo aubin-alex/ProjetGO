75ae304daef3c42e341d2322e839c640ab24a0c8
aubin-alex <aubin.rolin@eleves.ec-nantes.fr>
2015-12-03 10:04:03 +0100

    test de commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProjetGO
+# ProjetGO
---
+++ main.cpp
@@ -0,0 +1,34 @@
+/*
+ * File:   main.cpp
+ * Author: user
+ *
+ * Created on 3 décembre 2015, 09:05
+ */
+
+#include <cstdlib>
+#include<iostream>
+#include"gtest.h"
+#include"factoriel.h"
+
+using namespace std;
+
+/*
+ *
+ */
+
+int factoriel (int nombre){
+
+    int resultat=1;
+
+    for(int i=1;i<nombre+1;i++){
+        resultat=resultat*i;
+    }
+
+    return resultat;
+}
+
+TEST(factoriel_test, GereLesValeursPositives){
+    //EXPECT_EQ(1,factoriel(1));
+    //EXPECT_EQ(430, factoriel(5));
+    EXPECT_EQ(24, factoriel(4));
+}
