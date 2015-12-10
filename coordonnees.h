d98e1e0bf252b918d8038dac193a3eee66d5bd87
aubin-alex <aubin.rolin@eleves.ec-nantes.fr>
2015-12-10 11:54:16 +0100

    nouvelle classe coordonnees

---
+++ coordonnees.h
@@ -0,0 +1,14 @@
+#ifndef COORDONNEES_H_INCLUDED
+#define COORDONNEES_H_INCLUDED
+
+class Coordonees{
+    public:
+        int x,y;
+        Coordonees(int _x, int _y){
+            x=_x;
+            y=_y;
+        }
+};
+
+
+#endif // COORDONNEES_H_INCLUDED
