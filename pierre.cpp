40435c6e2237d31521d0a0842fb550de406afed6
JBHugo <Jean-Baptiste.Cliche@eleves.ec-nantes.fr>
2015-12-17 09:55:17 +0100

    Modifications de la fonction de test de liberté pour vérifier ques les coordonnées existent

--- pierre.cpp
+++ pierre.cpp
@@ -12,17 +12,28 @@ Pierre::Pierre(int _x, int _y, char col){
 // Vecteur renvoyant les coordonnées des libertés d'une pierre si elle existe
 vector<Coordonees> Pierre::test_liberte(Goban plateau){
     vector<Coordonees> VecLibertes;
-    if (plateau.jeu[getX()-1][getY()]==' '){
-        VecLibertes.push_back(Coordonees(getX()-1,getY()));
+
+    if(getX()-1>=0){
+        if (plateau.jeu[getX()-1][getY()]==' '){
+            VecLibertes.push_back(Coordonees(getX()-1,getY()));
+        }
     }
-    if (plateau.jeu[getX()+1][getY()]==' '){
-        VecLibertes.push_back(Coordonees(getX()+1,getY()));
+
+    if(getX()+1<TAILLE){
+        if (plateau.jeu[getX()+1][getY()]==' '){
+            VecLibertes.push_back(Coordonees(getX()+1,getY()));
+        }
     }
-    if (plateau.jeu[getX()][getY()-1]==' '){
-        VecLibertes.push_back(Coordonees(getX(),getY()-1));
+
+    if(getY()-1>=0){
+        if (plateau.jeu[getX()][getY()-1]==' '){
+            VecLibertes.push_back(Coordonees(getX(),getY()-1));
+        }
     }
-     if (plateau.jeu[getX()][getY()+1]==' '){
-        VecLibertes.push_back(Coordonees(getX(),getY()+1));
+    if(getY()+1<TAILLE){
+        if (plateau.jeu[getX()][getY()+1]==' '){
+            VecLibertes.push_back(Coordonees(getX(),getY()+1));
+        }
     }
 
     return VecLibertes;
