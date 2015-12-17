b04726d5f9e98ba571eb446f1434dbd0f725a43a
JBHugo <Jean-Baptiste.Cliche@eleves.ec-nantes.fr>
2015-12-17 09:37:41 +0100

    Retrait de la fonction pour rajouter la pierre à un groupe

--- pierre.h
+++ pierre.h
@@ -19,15 +19,15 @@ private:
 public:
     //Constructeur
     Pierre(int _x, int _y, char col);
-    void misegroupe(vector< Groupe > liste_groupe);
     //Destructeur
     ~Pierre();
     //Accesseurs
     int getX(){return x;}
     int getY(){return y;}
     char getColor(){return couleur;}
-    vector<Coordonees> test_liberte(Goban plateau);
     //Fonction pour savoir si la pierre est à proximité d'un groupe
+    vector<Coordonees> test_liberte(Goban plateau);
+
 
 };
 
